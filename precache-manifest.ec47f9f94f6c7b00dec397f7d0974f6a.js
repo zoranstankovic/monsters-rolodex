d796b9548ee8e6eb4a3107eb94ba00e15b7421db
Zoran <zoranstankovic24@gmail.com>
2019-08-10 14:03:02 +0200

    Updates

---
+++ precache-manifest.ec47f9f94f6c7b00dec397f7d0974f6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2aa5ba19289904b4befe55e600754d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fa2e148b0538068e67ed",
+    "url": "/monsters-rolodex/static/css/main.f35456bf.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "fa2e148b0538068e67ed",
+    "url": "/monsters-rolodex/static/js/main.15e657ca.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
